4e19715f751e01f4f1819c90cea5c6a73ccad8fb
Panagiotis <pgiagkoulas@gmail.com>
2019-10-06 14:41:07 +0200

    loading experiment parameters form file + adjustments
    args is now a dictionary
    exp1 is a mock file to test functionality
    NEEDS RESPONSIBLE HANDLING

--- a9_fake_news_simulator/io_utils.py
+++ a9_fake_news_simulator/io_utils.py
@@ -4,6 +4,11 @@ dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, './saved_experiments/')
 
 
+# helper function checks existence of given experiment name
+def experiment_exists(experiment_name):
+    return os.path.isfile(filename + experiment_name + '.txt')
+
+
 # casts parameter values to the appropriate type
 def cast_param_values(params):
     params['n_agents'] = int(params['n_agents'])
@@ -18,11 +23,11 @@ def cast_param_values(params):
 
 
 # loads parameters from given file
-def load_experiment_settings(experiment='default'):
+def load_experiment_settings(experiment_name='default'):
     # initialize parameters object
     params = dict()
     # read parameters from the text file
-    with open(filename+experiment+'.txt') as exp:
+    with open(filename + experiment_name + '.txt') as exp:
         for line in exp:
             (key, val) = line.split()
             params[key] = val
